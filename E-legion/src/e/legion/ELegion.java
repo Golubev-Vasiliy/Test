d0f0863b4d6503329f00c3282d853503567ebd13
Golubev Vasiliy <vase4ka346@gmail.com>
2015-08-15 06:39:12 +0300

    Создал двумерный массив для хранения в нем координат вершин прямоугольника

--- E-legion/src/e/legion/ELegion.java
+++ E-legion/src/e/legion/ELegion.java
@@ -1,21 +1,15 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package e.legion;
 
-/**
- *
- * @author User
- */
-public class ELegion {
 
-    /**
-     * @param args the command line arguments
-     */
+public class ELegion {
     public static void main(String[] args) {
-        // TODO code application logic here
+        int Mas[][];
+        int N;                      //Кол-во строк
+        
+        
+        Mas = new int[N][3];       //Mas[N][x1, y1, x2, y2]
+        
     }
     
 }
