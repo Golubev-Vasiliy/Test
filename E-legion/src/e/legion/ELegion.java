07d61e6f351f0c28f5c9025e833fc1c3c92ed676
Golubev Vasiliy <vase4ka346@gmail.com>
2015-08-15 17:47:56 +0300

    В программе реализованно получение аргументов, а так же проверка на количество передаваемых аргументов

--- E-legion/src/e/legion/ELegion.java
+++ E-legion/src/e/legion/ELegion.java
@@ -1,14 +1,21 @@
 
 package e.legion;
-
+import java.io.*;
 
 public class ELegion {
-    public static void main(String[] args) {
-        int Mas[][];
-        int N;                      //Кол-во строк
-        
-        
-        Mas = new int[N][3];       //Mas[N][x1, y1, x2, y2]
+    public static void main(String[] args) throws FileNotFoundException {
+        if(args.length==2)      //Проверка на кол-во передаваемых аргументов
+        {
+            int Mas[][];
+            int N;                      //Кол-во строк
+            File Input = new File(args[0]);
+            System.out.printf("%s\n", args[0]);
+            System.out.printf("%s\n", args[1]);
+            Mas = new int[N=99][3];       //Mas[N][x1, y1, x2, y2]           
+        }
+        else
+            System.out.print("Ошибка!\nКоличество передаваемых аргументов должно быть 2 ");
+
         
     }
     
