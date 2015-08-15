0b9c213f36dcf45aceecabda63944d77aefe7506
Golubev Vasiliy <vase4ka346@gmail.com>
2015-08-15 19:08:15 +0300

    Создание класса FileReader

--- E-legion/src/e/legion/ELegion.java
+++ E-legion/src/e/legion/ELegion.java
@@ -3,12 +3,13 @@ package e.legion;
 import java.io.*;
 
 public class ELegion {
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args) throws FileNotFoundException, IOException {
         if(args.length==2)      //Проверка на кол-во передаваемых аргументов
         {
             int Mas[][];
-            int N;                      //Кол-во строк
-            File Input = new File(args[0]);
+            int N, c;                      //Кол-во строк
+            File f1 = new File(args[0]);
+            FileReader Input = new FileReader(f1);
             System.out.printf("%s\n", args[0]);
             System.out.printf("%s\n", args[1]);
             Mas = new int[N=99][3];       //Mas[N][x1, y1, x2, y2]           
