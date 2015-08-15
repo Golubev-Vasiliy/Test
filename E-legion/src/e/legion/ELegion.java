636eb516ad25fa469e8ecf72be44a66c0ba4039f
Golubev Vasiliy <vase4ka346@gmail.com>
2015-08-16 02:09:27 +0300

    Реализована сортировка таким образом, что координата x1y1 - всегда обозначает левый угол у прямоугольника, а так же прямоугольники отсортированы по удаленности от оси Y

--- E-legion/src/e/legion/ELegion.java
+++ E-legion/src/e/legion/ELegion.java
@@ -39,6 +39,47 @@ public class ELegion {
                     System.out.printf("Mas[%d][%d]=%d ", j, i, Mas[j][i]);
                 System.out.println();
             }
+            
+            for(int j=0; j<N; j++)                //Сортировка - смысл в том, что        
+            {                                     //первые 2 числа массива - всегда левый угол      
+                int xx, yy;  
+                if (Mas[j][0]>Mas[j][2])
+                {
+                    xx=Mas[j][2];
+                    Mas[j][2]=Mas[j][0];
+                    Mas[j][0]=xx;
+                    yy=Mas[j][3];
+                    Mas[j][3]=Mas[j][1];
+                    Mas[j][1]=yy;
+                }    
+                System.out.println();
+            }
+            
+            int vx=100, x1,x2,y1,y2;                          //Переменна показывающая сколько раз за цикл менялись местами строки
+            while(vx!=0)
+            {
+                vx=0;
+                for(int j=0; j<N; j++)
+                {                                 
+                    if(j!=N-1)
+                    if (Mas[j][0]>Mas[j+1][0])                   //Сортировка на удаленность от оси Y
+                    {
+                        x1=Mas[j][0]; y1=Mas[j][1]; x2=Mas[j][2]; y2=Mas[j][3];
+                        Mas[j][0]=Mas[j+1][0]; Mas[j][2]=Mas[j+1][1];
+                        Mas[j][2]=Mas[j+1][2]; Mas[j][3]=Mas[j+1][3];
+                        Mas[j+1][0]=x1; Mas[j+1][1]=y1; Mas[j+1][2]=x2;
+                        Mas[j+1][3]=y2;
+                        vx++;
+                    }
+                }
+            }
+            
+            for(int j=0; j<N; j++)                //вывод массива на экран
+            {
+                for(int i=0; i<=3; i++)
+                    System.out.printf("Mas[%d][%d]=%d ", j, i, Mas[j][i]);
+                System.out.println();
+            }
         }
         else
             System.out.print("Ошибка!\nКоличество передаваемых аргументов должно быть 2 ");
